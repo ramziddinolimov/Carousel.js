0b181846a84c2e958615c45ffa785451061d5da5
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-12-21 23:21:51 +0500

    carousel finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,13 +12,14 @@ carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 // Buttons Listeners
 nextBtn.addEventListener("click", () => {
-    if (counter <= 0) return;
+    if (counter >= carouselImages.length - 1) return;
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter++;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 })
 
 prevBtn.addEventListener("click", () => {
+    if (counter <= 0) return;
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter--;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
